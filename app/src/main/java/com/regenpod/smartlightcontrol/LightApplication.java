72e97b1ba56c6a85de0fa436a167339439888d86
liming <15970060419lma>
2020-10-29 16:59:10 +0800

    第一次提交

---
+++ app/src/main/java/com/regenpod/smartlightcontrol/LightApplication.java
@@ -0,0 +1,10 @@
+package com.regenpod.smartlightcontrol;
+
+import android.app.Application;
+
+public class LightApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
