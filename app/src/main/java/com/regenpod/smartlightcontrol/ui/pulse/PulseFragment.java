dcd34790ade2b793b6633e81aee5e91f9170cce8
liming <15970060419lma>
2020-11-09 20:43:39 +0800

    计算类

--- app/src/main/java/com/regenpod/smartlightcontrol/ui/pulse/PulseFragment.java
+++ app/src/main/java/com/regenpod/smartlightcontrol/ui/pulse/PulseFragment.java
@@ -1,10 +1,8 @@
 package com.regenpod.smartlightcontrol.ui.pulse;
 
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.lm.common.adapter.BaseCommonViewHolder;
@@ -17,6 +15,9 @@ public class PulseFragment extends BaseFragment {
     private BaseCommonViewHolder baseCommonViewHolder;
     private PulseViewModel pulseViewModel;
     private OperateHelper ht660OperateHelper;
+    private OperateHelper ht850OperateHelper;
+    private OperateHelper dc660OperateHelper;
+    private OperateHelper dc850OperateHelper;
 
     @Override
     protected int getLayoutId() {
@@ -28,17 +29,67 @@ public class PulseFragment extends BaseFragment {
         baseCommonViewHolder = new BaseCommonViewHolder(view);
         pulseViewModel =
                 ViewModelProviders.of(this).get(PulseViewModel.class);
-        pulseViewModel.getText().observe(this, new Observer<String>() {
+
+        initHt660();
+        initHt850();
+        initDc660();
+        initDc850();
+        baseCommonViewHolder.setOnClickListener(R.id.img_ok, new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
+            public void onClick(View v) {
+                int ht660Progress = ht660OperateHelper.getProgress();
+                int ht850Progress = ht850OperateHelper.getProgress();
+                int dc660Progress = dc660OperateHelper.getProgress();
+                int dc850Progress = dc850OperateHelper.getProgress();
+
+                StringBuilder sb = new StringBuilder();
+                sb.append("ht660:" + ht660Progress);
+                sb.append("\nht850:" + ht850Progress);
+                sb.append("\ndc660:" + dc850Progress);
+                sb.append("\ndc850:" + dc660Progress);
 
             }
         });
+    }
 
+
+    private void initHt660() {
         ht660OperateHelper = new OperateHelper();
         ht660OperateHelper.init(baseCommonViewHolder.getTextView(R.id.tv_ht_660),
                 baseCommonViewHolder.getView(R.id.img_ht_add_660),
                 baseCommonViewHolder.getView(R.id.img_ht_less_660),
+                new OperateHelper.OperateListener() {
+                    @Override
+                    public int getAdd(int progress) {
+                        progress = progress + 50;
+                        if (progress > 2000) {
+                            progress = 2000;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public int getLess(int progress) {
+                        progress = progress - 50;
+                        if (progress < 1) {
+                            progress = 1;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public String showProgress(int progress) {
+                        return progress + "HZ";
+                    }
+                });
+        ht660OperateHelper.setProgress(200);
+    }
+
+    private void initHt850() {
+        ht850OperateHelper = new OperateHelper();
+        ht850OperateHelper.init(baseCommonViewHolder.getTextView(R.id.tv_ht_850),
+                baseCommonViewHolder.getView(R.id.img_ht_add_850),
+                baseCommonViewHolder.getView(R.id.img_ht_less_850),
                 new OperateHelper.OperateListener() {
                     @Override
                     public int getAdd(int progress) {
@@ -60,11 +111,77 @@ public class PulseFragment extends BaseFragment {
 
                     @Override
                     public String showProgress(int progress) {
-                        return progress+"HZ";
+                        return progress + "HZ";
                     }
                 });
-        ht660OperateHelper.setProgress(200);
+        ht850OperateHelper.setProgress(100);
     }
+
+    private void initDc660() {
+        dc660OperateHelper = new OperateHelper();
+        dc660OperateHelper.init(baseCommonViewHolder.getTextView(R.id.tv_dc_660),
+                baseCommonViewHolder.getView(R.id.img_dc_add_660),
+                baseCommonViewHolder.getView(R.id.img_dc_less_660),
+                new OperateHelper.OperateListener() {
+                    @Override
+                    public int getAdd(int progress) {
+                        progress = progress + 1;
+                        if (progress > 80) {
+                            progress = 80;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public int getLess(int progress) {
+                        progress = progress - 1;
+                        if (progress < 1) {
+                            progress = 1;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public String showProgress(int progress) {
+                        return progress + "%";
+                    }
+                });
+        dc660OperateHelper.setProgress(20);
+    }
+
+
+    private void initDc850() {
+        dc850OperateHelper = new OperateHelper();
+        dc850OperateHelper.init(baseCommonViewHolder.getTextView(R.id.tv_dc_850),
+                baseCommonViewHolder.getView(R.id.img_dc_add_850),
+                baseCommonViewHolder.getView(R.id.img_dc_less_850),
+                new OperateHelper.OperateListener() {
+                    @Override
+                    public int getAdd(int progress) {
+                        progress = progress + 1;
+                        if (progress > 80) {
+                            progress = 80;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public int getLess(int progress) {
+                        progress = progress - 1;
+                        if (progress < 1) {
+                            progress = 1;
+                        }
+                        return progress;
+                    }
+
+                    @Override
+                    public String showProgress(int progress) {
+                        return progress + "%";
+                    }
+                });
+        dc850OperateHelper.setProgress(30);
+    }
+
 
     @Override
     protected void initData() {
